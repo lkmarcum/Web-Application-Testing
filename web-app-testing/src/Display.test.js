66b289f465b5babc5c0665318227bfba76bf49d3
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-08 16:22:47 -0500

    added testing for initial state render of display.js

--- web-app-testing/src/Display.test.js
+++ web-app-testing/src/Display.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Display from "./Display";
 import { render } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+// import "jest-dom/extend-expect";
 
 describe("<Display />", () => {
   it("renders without crashing", () => {
     render(<Display />);
+  });
+
+  it("displays the strikes", () => {
+    const { getByTestId } = render(<Display />);
+    expect(getByTestId("strikes").textContent).toBe("0");
   });
 });
